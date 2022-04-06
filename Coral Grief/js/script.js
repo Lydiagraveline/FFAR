5a281f691613dd99a3d5b7bb71417273bb4ad9f3
Lydia <ldgravy@gmail.com>
2022-04-06 12:58:02 -0400

    completed intro text

--- Coral Grief/js/script.js
+++ Coral Grief/js/script.js
@@ -54,19 +54,55 @@ Description of draw()
 function draw() {
   background(250, 236, 222, 255);
 
+  //lineNum = 1;
+
   textDisplay = `${textJson.line[lineNum]}`;
+   fill(130, 124, 255);
+   textFont('Space Mono');
+    textAlign(CENTER, CENTER);
+
+
+   // fill(130, 124, 255);
+   // noStroke();
+   // textFont('Space Mono');
+   // textSize(18);
+   // textAlign(LEFT, CENTER);
+   // text(textDisplay, width/4, height/2);
 
   if (state === "intro") {
-    push();
-    textSize(100);
-    fill(130, 124, 255);
-    textAlign(CENTER);
-    text(textDisplay, width / 2, height / 2);
+    if (lineNum === 0){
+      push();
+      textSize(50);
+      text(textDisplay, width / 2, height / 2);
+      textSize(35);
+      text("begin", width - 70, height - 30);
+      pop();
 
-    textSize(25);
-    text("next", width - 100, height / 2);
+    }
+    else if (lineNum === 1){
+      push();
+      textSize(22);
+      text(textDisplay, width / 2, height / 2);
+      pop();
+    }
+    else {
+      push();
+      textSize(18);
+      textAlign(LEFT, CENTER);
+      text(textDisplay, width/6, height / 2);
+      pop();
+    }
 
-    pop();
+    // display the "next" and "back" text
+    if (lineNum > 0){
+      push();
+      textSize(35);
+      text("next", width - 70, height - 30);
+      text("back", 70, height - 30);
+      pop();
+    }
+
+
   }
 
   // draw the coral reef
